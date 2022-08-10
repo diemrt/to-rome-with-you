6ce61e2c4701caddf555d2ec98127402dc4fabc4
diego <diego.martignoni@abenergie.it>
2022-08-10 16:58:24 +0200

    fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'elm': '#211814',
-      'crete': '#C6B8AA',
-      'gasoline': '#023E55',
-    },
     extend: {},
   },
   plugins: [],

28617f63a867983d29b706ee426c4dfa8219e6e8
diego <diego.martignoni@abenergie.it>
2022-08-10 14:36:37 +0200

    fixing welcome page

--- src/components/common/Body.jsx
+++ src/components/common/Body.jsx
@@ -6,7 +6,6 @@ class Body extends Component {
         return (
             <div className={`static w-screen h-screen p-4 ${this.props.bgColor} ${this.props.textColor}`}>
                 {this.props.content}
-                <img className="absolute -left-24 -bottom-0 overflow-hidden" src="/images/adventure1.png" alt="adventure"></img>
             </div>
         );
     }
--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -8,14 +8,17 @@ class Welcome extends Component{
     render(){
         return (
             <Body bgColor="bg-crete" textColor="text-elm" content={(
-                <div className="grid grid-cols-4 gap-4">
-                    <div>01</div>
-                    <div>09</div>
-                    <div>09</div>
-                    <div>09</div>
-                    <div>09</div>
-                    <div>09</div>
-                </div> 
+                <div>  
+                    <div className="grid grid-cols-4 gap-4">
+                        <div>01</div>
+                        <div>09</div>
+                        <div>09</div>
+                        <div>09</div>
+                        <div>09</div>
+                        <div>09</div>
+                    </div> 
+                    <img className="absolute -left-24 -bottom-0" src="/images/adventure1.png" alt="adventure"></img>
+                </div>
             )}></Body>
         );
     }

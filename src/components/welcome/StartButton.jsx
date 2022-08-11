e80046ed449d581a98846e88d1f9934fb05c00a1
diego <diego.martignoni@abenergie.it>
2022-08-11 11:04:29 +0200

    adding logic welcome page

---
+++ src/components/welcome/StartButton.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+class StartButton extends Component {
+
+
+}
+
+export default StartButton;
--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Body from "../components/common/Body";
 import Header from "../components/common/Header";
+import Title from "../components/welcome/Title";
 
 class Welcome extends Component{
     constructor(props){
@@ -38,7 +39,11 @@ class Welcome extends Component{
                 <div></div>
                 ) : (
                 <div>      
-                    <Header logo={data.header.logo} progressBar={data.header.progressBar}></Header>                
+                    <Header logo={data.header.logo} progressBar={data.header.progressBar}></Header>
+                    <div className="flex flex-col justify-center p-3">
+                        <Title></Title>
+                        <small className="text-center font-bold">Inizia il {new Date(data.body.welcome.releaseDate).toLocaleDateString('it-It', {year: 'numeric', month: 'long', day: 'numeric'})}</small>
+                    </div>                
                     <img className="absolute -left-24 -bottom-0" src="/images/adventure1.png" alt="adventure"></img>
                 </div>
             )) }></Body>

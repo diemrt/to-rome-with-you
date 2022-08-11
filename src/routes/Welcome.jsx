47edbca93ce23a57cc64cb7d0c8c15a3f337912c
diego <diego.martignoni@abenergie.it>
2022-08-11 10:23:33 +0200

    using fect ajax

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,17 +1,47 @@
 import { Component } from "react";
 import Body from "../components/common/Body";
 import Header from "../components/common/Header";
-import Settings from '../data/welcome.json';
 
 class Welcome extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            data: {}
+        }
+    }
+
+    componentDidMount(){
+        fetch('data/welcome.json')
+        .then((r) => r.json())
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                data: result.data
+            });
+        },
+        (error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        })
+    }
+
     render(){
+        const { error, isLoaded, data } = this.state;
         return (
-            <Body bgColor={Settings.data.body.bgColor} textColor={Settings.data.body.textColor} content={(
+            <Body content={error ? (
+                <div>Qualcosa è andato storto</div>
+                ) : (!isLoaded ? (
+                <div></div>
+                ) : (
                 <div>      
-                    <Header logo={Settings.data.header.logo} progressBar={Settings.data.header.progressBar}></Header>                
+                    <Header logo={data.header.logo} progressBar={data.header.progressBar}></Header>                
                     <img className="absolute -left-24 -bottom-0" src="/images/adventure1.png" alt="adventure"></img>
                 </div>
-            )}></Body>
+            )) }></Body>
         );
     }
 }
